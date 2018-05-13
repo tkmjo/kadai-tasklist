8161c9589b8c546592624cd5911e6fc073126336
tkmjo <t.kamijo87395426@gmail.com>
2018-05-13 05:55:04 +0000

    bootstrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,11 +2,24 @@
 
 @section('content')
     <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
-    <p>状態: {{ $task->status }}</p>
-    <p>タスク: {{ $task->content }}</p>
-    {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $task->id]) !!}
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>状態</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+
+    {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 @endsection
